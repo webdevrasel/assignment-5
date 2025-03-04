0dd64a22ae9607eeec956b61345fe3de9eb2548f
Web Dev Rasel <webdevrasel@gmail.com>
2025-03-04 15:10:53 +0600

    Project complete

---
+++ JavaScript/question.js
@@ -0,0 +1,10 @@
+document.getElementById('back-main').addEventListener('click' , function(){
+    window.location.href = 'index.html';
+    
+})
+
+// document.getElementById('Discover').addEventListener('click',
+//     function () {
+//       window.location.href =  "question.html"
+//     }
+// )
